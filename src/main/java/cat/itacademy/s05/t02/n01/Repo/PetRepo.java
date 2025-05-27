43b71a7e04a2415936b4bf44b042f379ba35b64e
isaac diez <isaac.diez@proton.me>
2025-05-27 13:57:30 +0200

    Feat: added PetRepo

---
+++ src/main/java/cat/itacademy/s05/t02/n01/Repo/PetRepo.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t02.n01.Repo;
+
+import cat.itacademy.s05.t02.n01.model.Pet;
+import cat.itacademy.s05.t02.n01.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PetRepo extends JpaRepository<Pet, Integer> {
+    Optional<Pet> findById(String id);
+}
