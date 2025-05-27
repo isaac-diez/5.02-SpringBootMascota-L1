654ad00b1bedfb70ce9e1d5de38537da4c946f7c
isaac diez <isaac.diez@proton.me>
2025-05-27 13:57:51 +0200

    Feat: added LevelStrategy and StateStrategy

---
+++ src/main/java/cat/itacademy/s05/t02/n01/service/strategy/LevelStrategy.java
@@ -0,0 +1,18 @@
+package cat.itacademy.s05.t02.n01.service.strategy;
+
+public class LevelStrategy {
+
+
+//    public void alimentar() {
+//        this.nivelHambre = Math.max(0, nivelHambre - 25);
+//        this.higiene = Math.max(0, higiene - 10);
+//    }
+//
+//    public void jugar() {
+//        if(nivelEnergia > 20) {
+//            this.nivelFelicidad = Math.min(100, nivelFelicidad + 15);
+//            this.nivelEnergia -= 10;
+//        }
+//    }
+
+}
---
+++ src/main/java/cat/itacademy/s05/t02/n01/service/strategy/StateStrategy.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s05.t02.n01.service.strategy;
+
+public class StateStrategy {
+
+
+
+
+    public void verificarEvolucion() {
+//        if(diasVividos >= 30) this.etapaVida = EtapaVida.ANCIANO;
+//        else if(diasVividos >= 15) this.etapaVida = EtapaVida.ADULTO;
+    }
+}
