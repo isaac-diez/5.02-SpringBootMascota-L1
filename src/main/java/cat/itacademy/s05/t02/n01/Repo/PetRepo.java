6065b74e2f3d0440b0958a3433240eefa3e27a0a
isaac diez <isaac.diez@proton.me>
2025-05-28 12:50:33 +0200

    Update: changed to String from Integer repo arguments

--- src/main/java/cat/itacademy/s05/t02/n01/Repo/PetRepo.java
+++ src/main/java/cat/itacademy/s05/t02/n01/Repo/PetRepo.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PetRepo extends JpaRepository<Pet, Integer> {
+public interface PetRepo extends JpaRepository<Pet, String> {
     Optional<Pet> findById(String id);
 }
