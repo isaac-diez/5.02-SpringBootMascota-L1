173a11dbd8d657339f2bfe12ed106d80ab5479f6
isaac diez <isaac.diez@proton.me>
2025-05-28 12:49:32 +0200

    Update: cleared unused dependencies

--- src/main/java/cat/itacademy/s05/t02/n01/service/PetService.java
+++ src/main/java/cat/itacademy/s05/t02/n01/service/PetService.java
@@ -2,7 +2,9 @@ package cat.itacademy.s05.t02.n01.service;
 
 import cat.itacademy.s05.t02.n01.model.Pet;
 import cat.itacademy.s05.t02.n01.model.PetDTO;
+
 import cat.itacademy.s05.t02.n01.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface PetService {
     public Pet createPet(User user, PetDTO petDto);
