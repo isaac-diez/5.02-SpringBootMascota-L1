b686b0e95ecd268050fc77b9721e5df794c8e65a
isaac diez <isaac.diez@proton.me>
2025-05-29 11:23:53 +0200

    Update: refactored UserRepo first method to return an Optional<User>

--- src/main/java/cat/itacademy/s05/t02/n01/Repo/UserRepo.java
+++ src/main/java/cat/itacademy/s05/t02/n01/Repo/UserRepo.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
-    User findByUsername (String username);
+    Optional<User> findByUsername (String username);
 }
