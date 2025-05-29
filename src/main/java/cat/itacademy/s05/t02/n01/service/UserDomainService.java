5b887d21388695eeec971a94aa84fbde5726ceaf
isaac diez <isaac.diez@proton.me>
2025-05-29 12:06:31 +0200

    Update: refactored UserDomainService for returning Optional instead of User

--- src/main/java/cat/itacademy/s05/t02/n01/service/UserDomainService.java
+++ src/main/java/cat/itacademy/s05/t02/n01/service/UserDomainService.java
@@ -3,6 +3,7 @@ package cat.itacademy.s05.t02.n01.service;
 import cat.itacademy.s05.t02.n01.Repo.UserRepo;
 import cat.itacademy.s05.t02.n01.model.User;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +15,9 @@ import org.springframework.stereotype.Service;
             this.userRepo = userRepo;
         }
 
-        public User findUserByUsername(String username) {
-            User user = userRepo.findByUsername(username);
-
-            if (user == null) {
-                throw new EntityNotFoundException("Usuario no encontrado");
-            }
+        public User findUserByUsername(String username) throws UsernameNotFoundException {
+            User user = userRepo.findByUsername(username).
+                    orElseThrow(() -> new UsernameNotFoundException("Usuario '" + username + "' no encontrado"));
 
             return user;
     }
