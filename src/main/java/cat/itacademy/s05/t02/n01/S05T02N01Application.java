ec745d1734ee9ddc05dc40a7dcbbaa904c938d06
isaac diez <isaac.diez@proton.me>
2025-06-06 17:30:38 +0200

    Update: added @EnableScheduling to main Class

--- src/main/java/cat/itacademy/s05/t02/n01/S05T02N01Application.java
+++ src/main/java/cat/itacademy/s05/t02/n01/S05T02N01Application.java
@@ -3,8 +3,10 @@ package cat.itacademy.s05.t02.n01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class S05T02N01Application {
 
 	public static void main(String[] args) {
