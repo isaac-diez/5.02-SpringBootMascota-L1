c3de731962d9120441c179dc4da1e8b2c9d034e5
isaac diez <isaac.diez@proton.me>
2025-06-06 17:30:18 +0200

    Update: created findAllByEvolutionStateNot()

--- src/main/java/cat/itacademy/s05/t02/n01/Repo/PetRepo.java
+++ src/main/java/cat/itacademy/s05/t02/n01/Repo/PetRepo.java
@@ -1,13 +1,17 @@
 package cat.itacademy.s05.t02.n01.Repo;
 
+import cat.itacademy.s05.t02.n01.model.EvolutionState;
 import cat.itacademy.s05.t02.n01.model.Pet;
 import cat.itacademy.s05.t02.n01.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PetRepo extends JpaRepository<Pet, Integer> {
     Optional<Pet> findById(int id);
+
+    List<Pet> findAllByEvolutionStateNot(EvolutionState evolutionState);
 }
