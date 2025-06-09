a7a2a8b76d73c984ffa28592022ab2228a59e7ab
isaac diez <isaac.diez@proton.me>
2025-06-09 18:10:03 +0200

    Update: added EVOLVED_KID, EVOLVED_TEENAGER, EVOLVED_ADULT, EVOLVED_SENIOR

--- src/main/java/cat/itacademy/s05/t02/n01/model/EventType.java
+++ src/main/java/cat/itacademy/s05/t02/n01/model/EventType.java
@@ -1,5 +1,5 @@
 package cat.itacademy.s05.t02.n01.model;
 
 public enum EventType {
-    FOOD, PLAY, SICK, SLEEP
+    FOOD, PLAY, SICK, SLEEP, EVOLVED_KID, EVOLVED_TEENAGER, EVOLVED_ADULT, EVOLVED_SENIOR, DEAD
 }
