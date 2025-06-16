5cb7fc19528b8f8c2987858f58b5684be170a242
isaac diez <isaac.diez@proton.me>
2025-06-16 11:18:42 +0200

    Feat: added WebConfig class

---
+++ 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/config/WebConfig.java
@@ -0,0 +1,23 @@
+package cat.itacademy.s05.t02.n01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS en todos los endpoints
+                        .allowedOrigins("http://localhost:5173") // La URL de tu frontend React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
