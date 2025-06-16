7078045a75bf77e00589d1665c53ee1efaa3e709
isaac diez <isaac.diez@proton.me>
2025-06-16 13:57:21 +0200

    Update: modified to attend single pet requests

--- 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/Repo/PetRepo.java
+++ 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/Repo/PetRepo.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 @Repository
 public interface PetRepo extends JpaRepository<Pet, Integer> {
     Optional<Pet> findById(int id);
-
+    List<Pet> findAllByUserId(Integer userId);
     List<Pet> findAllByEvolutionStateNot(EvolutionState evolutionState);
 }
