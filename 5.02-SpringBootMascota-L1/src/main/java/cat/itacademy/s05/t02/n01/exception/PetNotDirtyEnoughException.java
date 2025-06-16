5a6b10d88d21446b2f2a228323d4d7463484e4b6
isaac diez <isaac.diez@proton.me>
2025-06-16 19:39:45 +0200

    Update: added PetNotDirtyEnoughException

---
+++ 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/exception/PetNotDirtyEnoughException.java
@@ -0,0 +1,5 @@
+package cat.itacademy.s05.t02.n01.exception;
+
+public class PetNotDirtyEnoughException extends RuntimeException {
+    public PetNotDirtyEnoughException(String message) { super(message); }
+}
