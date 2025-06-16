d099d643949391aa19245f6808b0cd601d61da5c
isaac diez <isaac.diez@proton.me>
2025-06-16 13:56:21 +0200

    Update: modified to accept OPTIONS HttpRequests

--- 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/security/SecurityConfig.java
+++ 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package cat.itacademy.s05.t02.n01.security;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -42,6 +43,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/login",
                                 "/auth/register",
                                 "/user/new",
