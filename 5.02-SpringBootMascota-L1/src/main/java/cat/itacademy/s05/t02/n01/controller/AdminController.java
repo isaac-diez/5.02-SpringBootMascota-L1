06b3e61348f52bd99fc1749ace2a379656649135
isaac diez <isaac.diez@proton.me>
2025-06-16 14:02:48 +0200

    Feat: updated to add /pets/all endpoint for Admin users

--- 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/controller/AdminController.java
+++ 5.02-SpringBootMascota-L1/src/main/java/cat/itacademy/s05/t02/n01/controller/AdminController.java
@@ -1,15 +1,45 @@
 package cat.itacademy.s05.t02.n01.controller;
 
+import cat.itacademy.s05.t02.n01.dto.PetDto;
+import cat.itacademy.s05.t02.n01.dto.PetMapper;
+import cat.itacademy.s05.t02.n01.model.Pet;
+import cat.itacademy.s05.t02.n01.service.PetService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admin")
 @SecurityRequirement(name = "bearerAuth")
 public class AdminController {
 
+    @Autowired
+    private PetService petService;
+    @Autowired
+    private PetMapper petMapper;
+
     @GetMapping("/dashboard")
     public String adminDashboard() {
         return "Welcome to the ADMIN dashboard!";
+    }
+
+    @GetMapping("/pets/all")
+    public ResponseEntity<List<PetDto>> getAllPetsForAdmin() {
+        List<Pet> allPets = petService.getAllPetsAsAdmin();
+
+        if (allPets.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        // Mapear la lista completa a DTOs
+        List<PetDto> allPetsDto = allPets.stream()
+                .map(petMapper::toDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(allPetsDto);
     }
 }
